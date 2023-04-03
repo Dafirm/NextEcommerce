2177a4b6fc97aeb132d1f4d81b4dd6b10bf30db5
dafirm <ayanfeoyeniyi@gmail.com>
2023-04-03 09:55:44 +0300

    setup frontend and backend commit

---
+++ ecommercenextjs/pages/_app.js
@@ -0,0 +1,14 @@
+import react from 'react'
+import "../styles/globals.css"
+// import { Layout } from '../components';
+// import { Toaster } from 'react-hot-toast';
+// import { StateContext } from '../context/StateContext'
+
+
+function MyApp({ Component, pageProps }) {
+    
+        <Component {...pageProps} />
+    
+}
+
+export default MyApp;
